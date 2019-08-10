f6ab1eed3f03618251a033d07982f260e1de103f
Projit <projit1234@hotmail.com>
2019-08-10 18:03:46 +0530

    Returning serialized pickle from rust without temp file

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ ffi = FFI()
 ffi.cdef("""
    typedef struct {const char* response;} ReqStruct;
    ReqStruct rust_get(const char*);
+   typedef struct {int length; const char* response;} RetStruct;
    void dict_pass(const char*, const char*);
+   RetStruct alt_dict_pass(const char*);
+   void rust_post(const char*, const char*);
 """)
 
 C = ffi.dlopen("target/release/librequest_export.dylib")
@@ -25,18 +28,30 @@ def read_string(bytes_string):
         counter += 1
     return strf.decode()
 
+def read_pickle_bytes(pointer, length):
+    arr = b''
+    for i in range(length):
+        arr += pointer[i]
+    return arr
+
+
 # a = C.rust_get(to_string("https://google.com"))
 # output = read_string(a.response)
 
 def function_call(function, data):
     inp_file = tempfile.NamedTemporaryFile()
-    out_file = tempfile.NamedTemporaryFile()
     with open(inp_file.name, 'wb') as handle:
         pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    function(to_string(inp_file.name), to_string(out_file.name))
-    with open(out_file.name, 'rb') as f:
-        return pickle.load(f)
+    val = function(to_string(inp_file.name))
+    obj  = pickle.loads(read_pickle_bytes(val.response, val.length))
+    print(obj['response'])
+
+def function_call_2(function, data):
+    serialized = pickle.dumps(data, protocol=pickle.HIGHEST_PROTOCOL)
+    text = ffi.new("char[]", serialized)
+    val = function(text)
+    obj  = pickle.loads(read_pickle_bytes(val.response, val.length))
+    print(obj['response'])
 
 data = {'url': 'https://google.com'}
-output = function_call(C.dict_pass, data)
-print(output)
+output = function_call(C.alt_dict_pass, data)
--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,19 @@ use serde_json as json;
 use serde_pickle as pickle;
 use std::collections::BTreeMap;
 use std::fs;
-use std::fs::File;
-use std::io::{Read};
+use std::fs::File; use std::io::{Read};
+use std::mem;
 
 #[repr(C)]
 pub struct ReqStruct {
     response: *const c_char
 }
 
+#[repr(C)]
+pub struct RetStruct {
+    length: i64,
+    response: *mut u8
+}
 
 #[no_mangle]
 pub extern "C" fn rust_get(link: *const c_char) -> ReqStruct {
@@ -36,19 +41,75 @@ pub extern "C" fn dict_pass(input_temp_file: *const c_char, output_temp_file: *c
     // File to read and write to
     let input_filename = unsafe { CStr::from_ptr(input_temp_file).to_str().expect("Not a valid UTF-8 string") };
     let output_filename = unsafe { CStr::from_ptr(output_temp_file).to_str().expect("Not a valid UTF-8 string") };
-    
+
     //Processing
     let reader: Box<Read> = Box::new(File::open(input_filename).unwrap());
     let decoded: json::Value = pickle::from_reader(reader).unwrap();
+    let mut map = BTreeMap::new();
 
     //Needs to be implemented
     let url = Url::parse(&decoded["url"].as_str().expect("Not a valid UTF-8 string")).unwrap();
     let resp = reqwest::get(url).unwrap().text().unwrap();
-    let mut map = BTreeMap::new();
     map.insert("response".to_string(), resp);
 
 
     //Processing
     let serialized = serde_pickle::to_vec(&map, true).unwrap();
     fs::write(output_filename, serialized).expect("Unable to write file");
+}
+
+
+#[no_mangle]
+pub extern "C" fn rust_post(input_temp_file: *const c_char, output_temp_file: *const c_char) {
+    // File to read and write to
+    let input_filename = unsafe { CStr::from_ptr(input_temp_file).to_str().expect("Not a valid UTF-8 string") };
+    let output_filename = unsafe { CStr::from_ptr(output_temp_file).to_str().expect("Not a valid UTF-8 string") };
+
+    //Processing
+    let reader: Box<Read> = Box::new(File::open(input_filename).unwrap());
+    let decoded: json::Value = pickle::from_reader(reader).unwrap();
+    let mut map = BTreeMap::new();
+
+    let data = &decoded["data"];
+    let url = Url::parse(&decoded["url"].as_str().expect("Not a valid UTF-8 string")).unwrap();
+    let client = reqwest::Client::new();
+    let mut res = client.post(url)
+        .json(&data)
+        .send().unwrap();
+
+    map.insert("response".to_string(), res.text().unwrap());
+    //Processing
+
+    let serialized = serde_pickle::to_vec(&map, true).unwrap();
+    fs::write(output_filename, serialized).expect("Unable to write file");
+}
+
+#[no_mangle]
+pub extern "C" fn alt_dict_pass(input_temp_file: *const c_char) -> RetStruct {
+    // File to read and write to
+    let input_filename = unsafe { CStr::from_ptr(input_temp_file).to_str().expect("Not a valid UTF-8 string") };
+
+    //Processing
+    let reader: Box<Read> = Box::new(File::open(input_filename).unwrap());
+    let decoded: json::Value = pickle::from_reader(reader).unwrap();
+    let mut map = BTreeMap::new();
+
+    //Needs to be implemented
+    let url = Url::parse(&decoded["url"].as_str().expect("Not a valid UTF-8 string")).unwrap();
+    let resp = reqwest::get(url).unwrap().text().unwrap();
+    map.insert("response".to_string(), resp);
+
+
+    //Processing
+    let mut serialized = serde_pickle::to_vec(&map, true).unwrap();
+    let x = serialized.len();
+    let p = serialized.as_mut_ptr();
+    unsafe {
+        mem::forget(serialized);
+        RetStruct{
+        length: x as i64,
+        response: p
+        }
+    }
+
 }
